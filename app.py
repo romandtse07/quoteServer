97f6984a1dbd7d0f9206ca35ab2d3c251e8d7b5b
NAILGUN <iamshalashaska01101101@gmail.com>
2021-06-20 10:30:09 -0700

    in hindsight taking out post

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ import io
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
 def main():
-    if request.method == 'POST':
-        service = request.headers.get('service')
-        margin = request.headers.get('margin')
-    else:
-        service = 'test'
-        margin = 0
-    dummy = pd.DataFrame([{'service':service, 'margin':margin}])
+    service = request.args.get('service')
+    margin = request.args.get('margin')
+    # in case for testing
+    service = 'test' if service is None else service
+    margin = 0 if margin is None else service
+    dummy = pd.DataFrame([{'service': service,
+                           'margin': margin}])
     excel_file = io.BytesIO()
     writer = pd.ExcelWriter(excel_file, engine='openpyxl')
     dummy.to_excel(writer, index=False)
     writer.close()
     excel_file.seek(0)
-    return send_file(excel_file, attachment_filename=f'service.xlsx', as_attachment=True)
+    return send_file(excel_file, attachment_filename=f'{service}.xlsx', as_attachment=True)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
