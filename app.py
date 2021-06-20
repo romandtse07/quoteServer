fc684ac4ae53b925a7bad8cc4fccbe94083840f9
NAILGUN <iamshalashaska01101101@gmail.com>
2021-06-19 21:17:46 -0700

    modified for test post method

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 import pandas as pd
 import io
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    dummy = pd.DataFrame([{'ugh':1, 'wut':2}])
+    if request.method == 'POST':
+        service = request.headers.get('service')
+        margin = request.headers.get('margin')
+    else:
+        service = 'test'
+        margin = 0
+    dummy = pd.DataFrame([{'service':service, 'margin':margin}])
     excel_file = io.BytesIO()
     writer = pd.ExcelWriter(excel_file, engine='openpyxl')
     dummy.to_excel(writer, index=False)
     writer.close()
     excel_file.seek(0)
-    return send_file(excel_file, attachment_filename='dummy.xlsx', as_attachment=True)
+    return send_file(excel_file, attachment_filename=f'service.xlsx', as_attachment=True)
 
 if __name__=='__main__':
     app.run(debug=True)
